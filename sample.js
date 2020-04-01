9e2ea521b91dfc221ea29834e4abf8f6b2e23c09
Indunil1992 <indunil@adroitlogic.com>
2020-04-01 11:18:07 +0530

    1ccc

--- sample.js
+++ sample.js
@@ -1,20 +1,5 @@
-let AWS = require('aws-sdk');
-const cognito_idp = new AWS.CognitoIdentityServiceProvider();
-
 exports.handler = async (event) => {
-    try {
-        let data = await cognito_idp.adminEnableUser({
-            UserPoolId: process.env.UserPoolId_cognitoIndunilpoolAp1,
-            Username: "indunil"
-        }).promise();
-console.log("dataa")
-console.log(data)
-    } catch (err) {
-        console.log("errrrrr")
-console.log(err)
-        // error handling goes here
-    };
 
 
-    return { "message": "Successfully executed" };
+    return { "message": "Successfully executed indu" };
 };
