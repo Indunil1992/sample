238757b3d32204f92ed4696843c95f9d5f0d9e68
Indunil1992 <indunil@adroitlogic.com>
2020-04-01 10:06:40 +0530

    0o

--- sample.js
+++ sample.js
@@ -6,7 +6,7 @@ exports.handler = async (event) => {
         let data = await cognito_idp.listUsers({
             UserPoolId: "us-east-1_WrZwPbTz1",
             AttributesToGet: ['sub', 'name', 'given_name', 'family_name', 'middle_name', 'nickname', 'preferred_username', 'profile', 'picture', 'website', 'email', 'email_verified', 'gender', 'birthdate', 'zoneinfo', 'locale', 'phone_number', 'phone_number_verified', 'address', 'updated_at', 'custom:timezone'],
-            Limit: 50
+            Limit: 100
         }).promise();
         console.log("dataa")
         console.log(data)
