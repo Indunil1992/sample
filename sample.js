25dea12974cabf68856183ee377ad0e61d78953c
Indunil1992 <indunil@adroitlogic.com>
2020-04-01 10:59:07 +0530

    12

--- sample.js
+++ sample.js
@@ -2,7 +2,6 @@ let AWS = require('aws-sdk');
 const cognito_idp = new AWS.CognitoIdentityServiceProvider();
 
 exports.handler = async (event) => {
-
     try {
         let data = await cognito_idp.adminEnableUser({
             UserPoolId: process.env.UserPoolId_cognitoIndunilpoolAp1,
@@ -15,6 +14,7 @@ console.log(data)
 console.log(err)
         // error handling goes here
     };
+
 
     return { "message": "Successfully executed" };
 };
